c4d5c2c2b2e80a2ddc45a6cae32a34d48177ca97
Chris Lewis <clewis@typenetwork.com>
2019-06-21 18:19:57 +0000

    get rid of logopogo download buttons

--- logopogo/logopogo.php
+++ logopogo/logopogo.php
@@ -1,11 +1,3 @@
 <figure id='pogologo' class="variable-demo-target" contenteditable spellcheck="false" data-style="Settings">
 	Brand Name
 </figure>
-<div id='save-buttons'>
-<!-- 			<h3>Save as:</h3> -->
-	<button class="button_default" id='save-svg'>Download SVG</button>
-<!--
-	<button class="button_default" id='save-pdf'>PDF</button>
-	<button class="button_default" id='save-ttf'>TTF</button>
--->
-</div>
