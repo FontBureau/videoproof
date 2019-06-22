6aa21cfce168f183b30827533dbe5b59571dc450
Chris Lewis <clewis@typenetwork.com>
2019-06-22 16:59:36 +0000

    show glyphs in defined group order instead of unicode

--- proof/proof.js
+++ proof/proof.js
@@ -9,6 +9,54 @@ document.addEventListener('DOMContentLoaded', function() {
 	var controls = $('#controls');
 	var proof = $('#proof-grid');
 	var glyphselect = $('#select-glyphs');
+	var glyphsort = "glyphset";
+	var currentFont;
+	
+	//find characters in the font that aren't in any of the defined glyph groups
+	function getOrphanChars() {
+		var definedGlyphs = {};
+		Array.from(getKnownGlyphs()).forEach(function(c) {
+			definedGlyphs[c] = true;
+		});
+		var result = "";
+		Object.keys(currentFont.tables.cmap.glyphIndexMap).forEach(function(u) {
+			var c = String.fromCodePoint(u);
+			if (!(c in definedGlyphs)) {
+				result += c;
+			}
+		});
+		return result;
+	}
+	
+	function getKnownGlyphs() {
+		var glyphset = '';
+		var addthing = function(thing) {
+			if (typeof thing === 'string') {
+				glyphset += thing;
+				return true;
+/*
+			} elseif (typeof thing === 'object' && 'chars' in thing) {
+				glyphset += thing.chars;
+				return true;
+*/
+			}
+			return false;
+		};
+		$.each(window.glyphsets, function(group, sets) {
+			if (addthing(sets)) {
+				return;
+			} else {
+				$.each(sets, function(i, set) {
+					addthing(set);
+				});
+			}
+		});
+		return glyphset;
+	}
+	
+	function getAllGlyphs() {
+		return getKnownGlyphs() + getOrphanChars();
+	}
 	
 	function getGlyphString() {
 		var groupSet = glyphselect.val().split('::');
@@ -20,47 +68,58 @@ document.addEventListener('DOMContentLoaded', function() {
 			} else if (groupSet[1] === 'concat') {
 				glyphset = [];
 				$.each(window.glyphsets[groupSet[0]], function(label, glyphs) {
-					glyphset.push(glyphs);
+					if (typeof glyphs === 'string') {
+						glyphset.push(glyphs);
+					}
 				});
 				glyphset = glyphset.join('').trim();
 			}
+		} else if (groupSet[0] === 'orphans') {
+			glyphset = getOrphanChars();
 		} else {
 			glyphset = window.glyphsets[groupSet[0]];
 		}
 
-		return glyphset;
+		return glyphset ? glyphset : getAllGlyphs();
 	}
 	
-	function filterGlyphs() {
+	function populateGrid() {
+		var cmap = currentFont.tables.cmap.glyphIndexMap;
 		var glyphset = getGlyphString();
-		
-		var showall = !glyphset;
-		var showglyphs = {};
-		
-		if (glyphset) {
-			Array.from(glyphset).forEach(function(c) {
-				showglyphs[c] = true;
-			});
+
+		if (typeof glyphset === 'object' && glyphset.chars && glyphset.feature) {
+			proof.css('font-feature-settings', '"' + glyphset.feature + '" 1');
+			glyphset = glyphset.chars;
+		} else {
+			proof.css('font-feature-settings', '');
 		}
-		proof.find('span').each(function() {
-			var c = this.textContent;
-			if (showall || showglyphs[c]) {
-				this.style.display = '';
-			} else {
-				this.style.display = 'none';
+
+		var unicodes = [];
+		var checkCmap = false;
+		switch (glyphsort) {
+			case 'glyph':
+				unicodes = Object.keys(cmap);
+				unicodes.sort(function(a, b) { return cmap[a] - cmap[b]; });
+				break;
+			case 'glyphset':
+				unicodes = Array.from(glyphset);
+				checkCmap = true;
+				break;
+			default:
+				unicodes = Object.keys(cmap);
+				unicodes.sort(function(a, b) { return a-b; });
+				break;
+		}
+
+		proof.empty();
+		unicodes.forEach(function(c) {
+			if (!checkCmap || c.codePointAt(0) in cmap) {
+				proof.append('<span>' + c + '</span>');
 			}
 		});
-		TNTools.doGridSize();
-	}
-	
-	function populateGrid(font) {
-		var gid;
-		proof.empty();
-		for (gid in font.tables.cmap.glyphIndexMap) {
-			proof.append('<span>' + String.fromCodePoint(gid) + '</span>');
-		}
+		
 		TNTools.slidersToElement();
-		filterGlyphs();
+		TNTools.doGridSize();
 	}
 	
 	$('#select-font')	.on('change', function() {
@@ -68,7 +127,8 @@ document.addEventListener('DOMContentLoaded', function() {
 		proof.html('<span style="font-size:1rem">Loading…</span>');
 
 		if (window.fontInfo[fonturl] && window.fontInfo[fonturl].fontobj) {
-			populateGrid(window.fontInfo[fonturl].fontobj);
+			window.font = currentFont = window.fontInfo[fonturl].fontobj;
+			populateGrid();
 		} else {
 			var url = '/fonts/' + fonturl + '.woff';
 			window.opentype.load(url, function (err, font) {
@@ -76,15 +136,11 @@ document.addEventListener('DOMContentLoaded', function() {
 					alert(err);
 					return;
 				}
-				window.fontInfo[fonturl].fontobj = font;
-				populateGrid(font);
+				window.font = window.fontInfo[fonturl].fontobj = currentFont = font;
+				populateGrid();
 			});
 		}
 	});
 	
-	$('#select-glyphs').on('change', function() {
-		//do some clever hiding and showing
-		
-		filterGlyphs();
-	});
+	$('#select-glyphs').on('change', populateGrid);
 });
