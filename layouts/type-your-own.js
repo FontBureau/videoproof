051497881b38d6765007982a5e3c4aa0fc86d3f5
Chris Lewis <clewis@typenetwork.com>
2019-08-02 18:16:42 +0000

    add extended checkbox to type your own

--- layouts/type-your-own.js
+++ layouts/type-your-own.js
@@ -2,8 +2,35 @@
 "use strict";
 VideoProof.registerLayout('type-your-own', {
 	'init': function(proof) {
+		
+		function updateProof() {
+			$(proof).find('span.extended-chars').remove();
+			if (document.getElementById('show-extended-glyphs').checked) {
+				var chars = Array.from(proof.textContent.trim());
+				var seen = {};
+				var extended = "";
+				chars.forEach(function(c) {
+					if (c in seen) {
+						return;
+					}
+					seen[c] = true;
+					if (c in window.glyphsets._extended) {
+						extended += " " + window.glyphsets._extended[c];
+					}
+				});
+				if (extended.length) {
+					var span = document.createElement('span');
+					span.className = 'extended-chars';
+					span.textContent = extended;
+					proof.appendChild(span);
+				}
+			}
+		}
+
 		proof.textContent = "Type your own";
 		proof.setAttribute('contenteditable', '');
+		$(proof).on('keyup input', updateProof);
+		$('#show-extended-glyphs').on('change', updateProof);
 	},
 	'deinit': function(proof) {
 		proof.removeAttribute('contenteditable');
